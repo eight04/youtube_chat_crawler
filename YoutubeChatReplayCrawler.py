a786d3be6d9d45122cf2a13a7d3493be75c43420
eight04 <eight04@gmail.com>
2020-05-15 12:10:06 +0800

    Fix: drop unrelated logic

--- YoutubeChatReplayCrawler.py
+++ YoutubeChatReplayCrawler.py
@@ -31,62 +31,39 @@ for iframe in soup.find_all("iframe"):
         next_url= iframe["src"]
 
 print(next_url)
-while(1):
+while True:
+    html = session.get(next_url, headers=headers)
+    soup = BeautifulSoup(html.text,"lxml")
+
+    # 次に飛ぶurlのデータがある部分をfind_allで探してsplitで整形
+    for scrp in soup.find_all("script"):
+        # breakpoint()
+        if scrp.string and "window[\"ytInitialData\"]" in scrp.string:
+            dict_str = ''.join(scrp.string.split(" = ")[1:])
+
+    # javascript表記なので更に整形. falseとtrueの表記を直す
+    dict_str = dict_str.replace("false","False")
+    dict_str = dict_str.replace("true","True")
+
+    # 辞書形式と認識すると簡単にデータを取得できるが, 末尾に邪魔なのがあるので消しておく（「空白2つ + \n + ;」を消す）
+    dict_str = dict_str.rstrip("  \n;")
+    dict_str = RE_EMOJI.sub(r'', dict_str)
+    # 辞書形式に変換
+    dics = ast.literal_eval(dict_str)
 
     try:
-        html = session.get(next_url, headers=headers)
-        soup = BeautifulSoup(html.text,"lxml")
-        
-
-        # 次に飛ぶurlのデータがある部分をfind_allで探してsplitで整形
-        for scrp in soup.find_all("script"):
-            if "window[\"ytInitialData\"]" in scrp.text:
-                dict_str = ''.join(scrp.text.split(" = ")[1:])
-
-        # javascript表記なので更に整形. falseとtrueの表記を直す
-        dict_str = dict_str.replace("false","False")
-        dict_str = dict_str.replace("true","True")
-
-        # 辞書形式と認識すると簡単にデータを取得できるが, 末尾に邪魔なのがあるので消しておく（「空白2つ + \n + ;」を消す）
-        dict_str = dict_str.rstrip("  \n;")
-        dict_str = RE_EMOJI.sub(r'', dict_str)
-        # 辞書形式に変換
-        dics = ast.literal_eval(dict_str)
-
         # "https://www.youtube.com/live_chat_replay?continuation=" + continue_url が次のlive_chat_replayのurl
         continue_url = dics["continuationContents"]["liveChatContinuation"]["continuations"][0]["liveChatReplayContinuationData"]["continuation"]
-        print(continue_url)
-        next_url = "https://www.youtube.com/live_chat_replay?continuation=" + continue_url
-        # dics["continuationContents"]["liveChatContinuation"]["actions"]がコメントデータのリスト。先頭はノイズデータなので[1:]で保存
-        for samp in dics["continuationContents"]["liveChatContinuation"]["actions"][1:]:
-            comment_data.append(str(samp)+"\n")
+    except KeyError:
+        break
+        
+    print(continue_url)
+    next_url = "https://www.youtube.com/live_chat_replay?continuation=" + continue_url
 
-    # next_urlが入手できなくなったら終わり
-    except requests.ConnectionError as e:
-        print("Connection Error")
-        continue
-    except requests.HTTPError as e:
-        print("HTTPError")
-        break
-    except requests.Timeout as e:
-        print("Timeout")
-        continue
-    except requests.exceptions.RequestException as e:
-        print(e)
-        break
-    except KeyError as e:
-        print("KeyError")
-        print(e)
-        break
-    except SyntaxError as e:
-        print("SyntaxError")
-        print(e)
-        continue
-    except KeyboardInterrupt as e:
-        break
-    except :
-        print("Unexpected error:" + str(sys.exc_info()[0]))
-
+    # dics["continuationContents"]["liveChatContinuation"]["actions"]がコメントデータのリスト。先頭はノイズデータなので[1:]で保存
+    for samp in dics["continuationContents"]["liveChatContinuation"]["actions"][1:]:
+        comment_data.append(str(samp)+"\n")
+        
 # comment_data.txt にコメントデータを書き込む
 with open(title+".json", mode='w', encoding="utf-8") as f:
     f.writelines(comment_data)
